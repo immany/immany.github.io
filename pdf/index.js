63d35dad18510b15b5d6a318aec45e8b913fc5e0
Masaru Nakayama <nakayama@adobe.com>
2020-08-06 17:44:24 +0900

    Update index.js

--- pdf/index.js
+++ pdf/index.js
@@ -35,22 +35,8 @@ document.addEventListener("adobe_dc_view_sdk.ready", function () {
             /* file name */
             fileName: "Bodea Brochure.pdf"
         }
-    }, {};
+    }, {});
 
-    /* Register the callback to receive the events */
-    adobeDCView.registerCallback(
-        /* Type of call back */
-        AdobeDC.View.Enum.CallbackType.EVENT_LISTENER,
-        /* call back function */
-        function (event) {
-            console.log(event);
-        },
-        /* options to control the callback execution */
-        {
-            /* Enable PDF analytics events on user interaction. */
-            enablePDFAnalytics: true,
-        }
-    );
     /* Register the callback to receive the events */
     adobeDCView.registerCallback(
         /* Type of call back */
