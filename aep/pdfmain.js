a2c310f5de833165c4239ae674d0602affc47f9b
Masaru Nakayama <nakayama@adobe.com>
2024-06-20 12:01:04 +0900

    Update pdfmain.js

--- aep/pdfmain.js
+++ aep/pdfmain.js
@@ -51,7 +51,7 @@ document.addEventListener("adobe_dc_view_sdk.ready", function () {
         /* call back function */
         function (event) {
             console.log(event);
-            document.dispatchEvent(event);
+            document.addEventListener(event);
         },
         /* options to control the callback execution */
         {
