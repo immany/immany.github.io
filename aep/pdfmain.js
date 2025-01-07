eadeb507b027329d4594cc10afddfabc9d734283
Masaru Nakayama <nakayama@adobe.com>
2025-01-07 15:46:03 +0900

    Update pdfmain.js

--- aep/pdfmain.js
+++ aep/pdfmain.js
@@ -16,16 +16,23 @@ document.addEventListener("adobe_dc_view_sdk.ready", function () {
         /* Pass your registered client id */
         clientId: "84d17b3eb8564a30b8dd6020a66016dd",
         /* Pass the div id in which PDF should be rendered */
-        divId: "adobe-dc-view",
+        divId: "adobe-dc-view"
     });
 
+    /*Pass the events to receive */
+    const eventOptions = {
+      //Pass the PDF analytics events to receive.
+      //If no event is passed in listenOn, then all PDF analytics events will be received.
+      listenOn: [AdobeDC.View.Enum.Events.PDF_VIEWER_OPEN, AdobeDC.View.Enum.PDFAnalyticsEvents.PAGE_VIEW, AdobeDC.View.Enum.PDFAnalyticsEvents.DOCUMENT_OPEN, AdobeDC.View.Enum.PDFAnalyticsEvents.TEXT_SEARCH],
+      enablePDFAnalytics: true
+    };
     /* Invoke the file preview API on Adobe DC View object */
     adobeDCView.previewFile({
         /* Pass information on how to access the file */
         content: {
             /* Location of file where it is hosted */
             location: {
-                url: "https://acrobatservices.adobe.com/view-sdk-demo/PDFs/Bodea Brochure.pdf",
+                url: "https://acrobatservices.adobe.com/view-sdk-demo/PDFs/Bodea Brochure.pdf"
                 /*
                 If the file URL requires some additional headers, then it can be passed as follows:-
                 headers: [
@@ -35,7 +42,7 @@ document.addEventListener("adobe_dc_view_sdk.ready", function () {
                     }
                 ]
                 */
-            },
+            }
         },
         /* Pass meta data of file */
         metaData: {
@@ -43,13 +50,6 @@ document.addEventListener("adobe_dc_view_sdk.ready", function () {
             fileName: "Bodea Brochure.pdf"
         }
     }, {embedMode: "IN_LINE"});
-    
-    const eventOptions = {
-      //Pass the PDF analytics events to receive.
-      //If no event is passed in listenOn, then all PDF analytics events will be received.
-      listenOn: [ AdobeDC.View.Enum.Events.PDF_VIEWER_READY, AdobeDC.View.Enum.PDFAnalyticsEvents.PAGE_VIEW, AdobeDC.View.Enum.PDFAnalyticsEvents.DOCUMENT_OPEN, AdobeDC.View.Enum.PDFAnalyticsEvents.TEXT_SEARCH],
-      //enablePDFAnalytics: true
-    };
 
     /* Register the callback to receive the events */
     adobeDCView.registerCallback(
@@ -58,10 +58,12 @@ document.addEventListener("adobe_dc_view_sdk.ready", function () {
         /* call back function */
         function (event) {
             console.log(event);
-            if (event.type == "PDF_VIEWER_READY"){
+            if (event.type == "PDF_VIEWER_OPEN"){
                 adobeDataLayer.push({"event":event.type});
             }else if (event.type == "DOCUMENT_OPEN"){
                 adobeDataLayer.push({"event": event.type, "pdf":{"FileName": event.data.fileName}});
+            else if (event.type == "PAGE_VIEW"){
+                adobeDataLayer.push({"event": event.type, "pdf":{"FileName": event.data.fileName, "pageNumber": event.data.pageNumber}});
             }else if(event.type == "TEXT_SEARCH"){
                 adobeDataLayer.push({"event": event.type, "pdf":{"FileName": event.data.fileName, "textSearch": event.data.searchedText}});
             }
