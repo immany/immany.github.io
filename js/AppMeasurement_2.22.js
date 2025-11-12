864ec77cb635beea599e2fcbab93f72ac174fd6d
Masaru Nakayama <nakayama@adobe.com>
2025-11-12 14:37:36 +0900

    Update AppMeasurement_2.22.js

--- js/AppMeasurement_2.22.js
+++ js/AppMeasurement_2.22.js
@@ -24,8 +24,8 @@ changes to how your visitor data is collected.  Changes should only be
 made when instructed to do so by your account manager.*/
 s.debugTracking=true;
 s.visitorNamespace="adobemcc024";
-s.trackingServer="adobemcc024.data.adobedc.net";
-//s.trackingServer="adobemcc024.sc.omtrdc.net";
+//s.trackingServer="adobemcc024.data.adobedc.net";
+s.trackingServer="adobemcc024.sc.omtrdc.net";
 //s.visitorID = "f28cea7f-598f-4293-b249-0ae153a564e4";
 
 s.usePlugins=true;
